bbce564823ca3c5813225a2383ca010422ebf1b2
Pedro Augusto <pagb.pedro@hotmail.com>
2025-05-08 13:27:00 -0300

    feat: add is wallet owner permission

--- backend/transaction/api/permissions.py
+++ backend/transaction/api/permissions.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
 from rest_framework.views import APIView
@@ -16,6 +17,22 @@ class IsAdminOrWalletOwner(BasePermission):  # type: ignore
 
         if request.user and request.user.is_staff:
             return True
+
+        wallet = self.service.get_wallet(wallet_pk=wallet_pk)
+        return bool(wallet.user == request.user) if wallet else False
+
+
+class IsWalletOwner(BasePermission):
+    service = TransactionServices
+    message = "You are not the owner of this wallet."
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        wallet_pk = view.kwargs.get("pk") or request.data["to_wallet"]["number"]
+        if not wallet_pk:
+            return False
+
+        if isinstance(request.user, AnonymousUser):
+            return False
 
         wallet = self.service.get_wallet(wallet_pk=wallet_pk)
         return bool(wallet.user == request.user) if wallet else False
