52e5e3a3019d834c1424620f0a59689ba31e315c
Pedro Augusto <pagb.pedro@hotmail.com>
2025-05-08 13:35:03 -0300

    test: add unittest for transfer service

--- backend/transaction/tests/services/test_transaction_services.py
+++ backend/transaction/tests/services/test_transaction_services.py
@@ -40,3 +40,39 @@ class TestTransactionServiceDeposit(unittest.TestCase):
 
         self.assertEqual(service.transaction.status, TransactionStatus.FAILURE)
         self.assertEqual("The amount value can not be less or equal 0", vde.exception.message)
+
+
+class TestTransactionServiceTransfer(unittest.TestCase):
+    def setUp(self):
+        self.from_wallet = WalletFactory.create(balance=1000)
+        self.to_wallet = WalletFactory.create()
+        self.transaction_service = TransactionServices
+
+    def test_transfer_without_from_wallet_raises_validation_error(self):
+        service = TransactionServices(self.to_wallet.user, self.to_wallet, amount=100)
+
+        with self.assertRaises(ValidationError) as vde:
+            _ = service.transfer()
+
+        self.assertEqual(service.transaction.status, TransactionStatus.FAILURE)
+        self.assertEqual("Transfer transaction can not be perform without from wallet", vde.exception.message)
+
+    def test_transfer_without_enouth_balance_raises_validation_error(self):
+        service = TransactionServices(self.to_wallet.user, self.to_wallet, self.from_wallet, 1100)
+
+        with self.assertRaises(ValidationError) as vde:
+            _ = service.transfer()
+
+        self.assertEqual(service.transaction.status, TransactionStatus.FAILURE)
+        self.assertEqual("Not enouth balance on wallet to transfer", vde.exception.message)
+
+    def test_transfer_to_other_wallet_update_wallets_balance(self):
+        to_wallet_balance = self.to_wallet.balance
+        from_wallet_balance = self.from_wallet.balance
+
+        service = TransactionServices(self.to_wallet.user, self.to_wallet, self.from_wallet, 150)
+        transaction = service.transfer()
+
+        self.assertEqual(transaction.status, TransactionStatus.SETTLED)
+        self.assertEqual(service._to_wallet.balance, Decimal(to_wallet_balance + 150).quantize(Decimal("1.01")))
+        self.assertEqual(service._from_wallet.balance, Decimal(from_wallet_balance - 150).quantize(Decimal("1.01")))
