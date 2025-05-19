b25af554934a526271abcd186efd98d580a964df
Pedro Augusto <pagb.pedro@hotmail.com>
2025-05-19 18:55:35 -0300

    refactor: remove isadminorwalletowner permission

--- backend/transaction/api/permissions.py
+++ backend/transaction/api/permissions.py
@@ -6,22 +6,6 @@ from rest_framework.views import APIView
 from transaction.services import TransactionServices
 
 
-class IsAdminOrWalletOwner(BasePermission):
-    service = TransactionServices
-    message = "You are not the owner of this wallet."
-
-    def has_permission(self, request: Request, view: APIView) -> bool:
-        wallet_pk = view.kwargs.get("pk") or request.data["to_wallet"]["number"]
-        if not wallet_pk:
-            return False
-
-        if request.user and request.user.is_staff:
-            return True
-
-        wallet = self.service.get_wallet(wallet_pk=wallet_pk)
-        return bool(wallet.user == request.user) if wallet else False
-
-
 class IsWalletOwner(BasePermission):
     service = TransactionServices
     message = "You are not the owner of this wallet."
