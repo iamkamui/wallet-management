3586bcf82bf6d9474666edeec2c91bb57c93df48
Pedro Augusto <pagb.pedro@hotmail.com>
2025-05-19 15:37:16 -0300

    refactor: add date range filter on transaction filter

--- backend/transaction/filters.py
+++ backend/transaction/filters.py
@@ -1,12 +1,20 @@
 import django_filters
+from django.db.models import Q, QuerySet
 
-from transaction.models import Transaction
+from transaction.models import Transaction, Wallet
 
 
 class TransactionFilter(django_filters.FilterSet):
-    # start_date = django_filters.DateFilter(field_name="created_at", lookup_expr="gt")
-    # end_date = django_filters.DateFilter(field_name="created_at", lookup_expr="lt")
+    wallet = django_filters.CharFilter(method="filter_wallet_transactions")
+    start_date = django_filters.DateFilter(field_name="created_at", lookup_expr="gt")
+    end_date = django_filters.DateFilter(field_name="created_at", lookup_expr="lt")
+
+    def filter_wallet_transactions(
+        self, queryset: QuerySet[Transaction], name: str, value: Wallet
+    ) -> QuerySet[Transaction]:
+        filter = queryset.filter(Q(from_wallet__number=value) | Q(to_wallet__number=value))
+        return filter
 
     class Meta:
         model = Transaction
-        fields = ("from_wallet", "to_wallet")
+        fields = ()
