457de0fe8794f3e8ead01aab734a0ea088f90318
Pedro Augusto <pagb.pedro@hotmail.com>
2025-05-20 07:59:13 -0300

    feat: add drf spectacular redoc router

---
+++ backend/wallet_management/config/drf_spectacular.py
@@ -0,0 +1,9 @@
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Wallet Management Challenge",
+    "DESCRIPTION": "This project is a wallet management developed to improve backend skills",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_DIST": "SIDECAR",
+    "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
+    "REDOC_DIST": "SIDECAR",
+}
--- backend/wallet_management/config/rest_framework.py
+++ backend/wallet_management/config/rest_framework.py
@@ -1,5 +1,6 @@
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
     "TEST_REQUEST_RENDERER_CLASSES": [
         "rest_framework.renderers.MultiPartRenderer",
